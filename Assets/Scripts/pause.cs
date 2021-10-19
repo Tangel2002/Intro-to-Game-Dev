7bd014278912991cca0f1c2b29e79a0800e11b74
Tangel2002 <tangel@ltu.edu>
2021-10-19 10:39:55 -0400

    added pause

---
+++ Assets/Scripts/pause.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool isPaused = false;
+    public GameObject pauseUI;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            togglePause();
+        }
+    }
+    public void togglePause()
+    {
+        if (isPaused)
+        {
+            pauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+        else
+        {
+            
+            Time.timeScale = 0;
+            pauseUI.SetActive(true);
+        }
+
+        isPaused = !isPaused;
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
