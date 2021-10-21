491e665219887fa4b93e42be5bc3b6d170a178d5
Tangel2002 <tangel@ltu.edu>
2021-10-21 10:30:49 -0400

    added sound to menu button

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class menu : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public AudioSource blip;
     public void LoadLevel(string name)
     {
+        blip.Play();
         SceneManager.LoadScene(name);
+        
     }
     public void Quit()
     {
